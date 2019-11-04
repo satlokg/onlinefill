0c5a486282db463c5bb46de22e1350d4ab53fe36
Emma Paris <satlok.vipra@gmail.com>
2019-11-04 13:19:57 +0530

    report

--- app/Http/Controllers/Admin/ReportController.php
+++ app/Http/Controllers/Admin/ReportController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Task;
+use App\User;
 
 class ReportController extends Controller
 {
