078dea321959788c92e799a48fb1b6555b34b95c
Emma Paris <satlok.vipra@gmail.com>
2019-11-05 13:19:57 +0530

    jkd

--- resources/views/admin/report/timeline.blade.php
+++ resources/views/admin/report/timeline.blade.php
@@ -1,11 +1,12 @@
 <div class="pull-right">
+	<a href="{{route('admin.report.todayReport')}}" class="btn btn-sm btn-info pull-right"><i class="fa fa-refresh" aria-hidden="true"></i></a>
 <div class="col-sm-3 pull-right">
 	<form action="" method="get" class="form-horizontal">
 	<div class="input-group input-group-sm">
 		
 		<input autocomplete="off" type="text" class="form-control pull-right" id="datepicker" name="date">
 	    <span class="input-group-btn">
-	      <button type="submit" class="btn btn-info btn-flat">Go!</button>
+	      <button type="submit" class="btn btn-info btn-flat"><i class="fa fa-search" aria-hidden="true"></i></button>
 	    </span>
 	   
 	</div>
