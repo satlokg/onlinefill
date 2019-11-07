a761cedf5be2d1be119b6c18f45a1d240f0f0708
Emma Paris <satlok.vipra@gmail.com>
2019-11-07 11:20:50 +0530

    df

--- resources/views/admin/report/detail.blade.php
+++ resources/views/admin/report/detail.blade.php
@@ -72,7 +72,7 @@
                 {
                     title : '{{ $task[0]->AddPlayTime($task->pluck('hours')) }} Hours',
                     start : '{{ $date }}',
-                    url : '{{ route('user.report.detail',["date"=>$date]) }}'
+                    url : '{{ route('admin.user.report.detail',["date"=>$date]) }}'
                 },
                 @endforeach
             ]
